c2d07962ef86a8240ebf83e37ca3938848b3e3f7
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-06-22 17:12:55 +0300

    finish until docker

---
+++ app_33/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Glass
+from .serializers import PostSerializer
+
+# Create your views here.
+
+
+class PostsList(ListCreateAPIView):
+    queryset = Glass.objects.all()
+    serializer_class = PostSerializer
+
+class PostsDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Glass.objects.all()
+    serializer_class = PostSerializer
